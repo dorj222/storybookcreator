c291726898a3c521cb076acc8409cceff343b158
dorj222 <dorj222@gmail.com>
2024-01-25 15:10:52 +0100

    update diffusion_model

--- storybook/llm_models/diffusion_model.py
+++ storybook/llm_models/diffusion_model.py
@@ -3,6 +3,7 @@ from PIL import Image
 import torch
 import json
 import os
+import gc
 
 config_path = os.path.join(os.path.dirname(__file__), "../../img2img_models", "config.json")
 # Load configuration from config.json
@@ -10,11 +11,17 @@ with open(config_path, "r") as config_file:
     config = json.load(config_file)
 
 def run(image, prompt):
+    torch.cuda.empty_cache()
     pipe = StableDiffusionXLImg2ImgPipeline.from_single_file(config["realcartoon_path"],
                                                              strength=config["strength"],
                                                              guidance_scale=config["guidance_scale"],
                                                              num_inference_steps=config["num_inference_steps"]).to("cuda")
-    pipe.load_lora_weights(config["lora_path"])
+    # pipe.load_lora_weights(config["lora_path"])
     image = pipe(prompt, image=image, strength=config["strength"]).images[0]
     image.show()
+    # Immediately delete the pipe object
+    del pipe
+    # Perform garbage collection
+    gc.collect()
+    torch.cuda.empty_cache()
     return image
