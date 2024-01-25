112dfad05bc263e589e1f38557a4d7559414d771
dorj222 <dorj222@gmail.com>
2024-01-25 14:00:42 +0100

    update requirements file and add a configure file for diffusion model

--- storybook/llm_models/diffusion_model.py
+++ storybook/llm_models/diffusion_model.py
@@ -1,21 +1,20 @@
 from diffusers import StableDiffusionXLImg2ImgPipeline
 from PIL import Image
-import torch 
-#configuration - todo: setup proper config file
-model_path = "image2image/models/realcartoonXL_v6.safetensors"
-lora_path = "image2image/models/picbookXLloraV1.safetensors"
-strength = 0.80
-config = "realcartoon" # | "XL" | "realcartoon"
+import torch
+import json
+import os
 
+config_path = os.path.join(os.path.dirname(__file__), "../../img2img_models", "config.json")
+# Load configuration from config.json
+with open(config_path, "r") as config_file:
+    config = json.load(config_file)
 
 def run(image, prompt):
-    if config == "turbo":
-        model_path = "image2image/models/sd_xl_turbo_1.0.safetensors"
-    elif config == "realcartoon":
-        model_path = "image2image/models/realcartoonXL_v6.safetensors"
-    print("Generate picture for prompt: "+prompt)
-    pipe = StableDiffusionXLImg2ImgPipeline.from_single_file(model_path,strength=strength, guidance_scale=0.0, num_inference_steps=2).to("cuda") 
-    pipe.load_lora_weights(lora_path)
-    image = pipe(prompt, image=image, strength=strength).images[0]
-    image.show() 
+    pipe = StableDiffusionXLImg2ImgPipeline.from_single_file(config["realcartoon_path"],
+                                                             strength=config["strength"],
+                                                             guidance_scale=config["guidance_scale"],
+                                                             num_inference_steps=config["num_inference_steps"]).to("cuda")
+    pipe.load_lora_weights(config["lora_path"])
+    image = pipe(prompt, image=image, strength=config["strength"]).images[0]
+    image.show()
     return image
