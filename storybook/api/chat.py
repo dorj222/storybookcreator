8e6543d84f56694ea0833a4417dcfcd6833a6937
Gabriel <139224059+DorianDreams@users.noreply.github.com>
2024-04-12 15:13:51 +0200

    Use InstructBLIP for chat endpoints

--- storybook/api/chat.py
+++ storybook/api/chat.py
@@ -10,8 +10,8 @@ from storybook.llm_models.tiny_llama import generate_title
 from storybook.llm_models.seamless import translate_text
 from storybook.llm_models.blip import generate_image_description
 # LLM Model BLIP for description text generation
-from storybook.llm_models.blip import generate_image_caption, generate_initial_text
-from storybook.llm_models.blip_instruct import complete_sentence
+#from storybook.llm_models.blip import generate_image_caption, generate_initial_text
+from storybook.llm_models.blip_instruct import complete_sentence, continue_story, image_caption
 
 router = Router()
 @router.post("/titles")
@@ -24,8 +24,20 @@ def generate_translations(request, data: TranslateTextSchema):
     title_text = translate_text(data.user_input, data.tgt_lang)
     return JsonResponse({'generated_text': title_text})
 
-@router.post("/storysentences")
-def generate_complete_sentence(request, image: UploadedFile = File(...), prompt: Optional[str] = None,  chapter_index: Optional[str] = None):
+@router.post("/sentences")
+def generate_story_continuationi(request, image: UploadedFile = File(...), 
+                               prompt: Optional[str] = None, 
+                               chapter_index: Optional[str] = None):
+    # image to text caption generation
+    generated_description = continue_story(pil_image=image,prompt=prompt) 
+    response_data = {
+        "generated_description": generated_description
+    } 
+    return JsonResponse(response_data)
+
+@router.post("/completions")
+def generate_complete_sentence(request, image: UploadedFile = File(...), 
+                               prompt: Optional[str] = None):
     # image to text caption generation
     generated_description = complete_sentence(pil_image=image,prompt=prompt) 
     response_data = {
@@ -35,5 +47,5 @@ def generate_complete_sentence(request, image: UploadedFile = File(...), prompt:
 
 @router.post("/captions")
 def create_image_caption(request, image: UploadedFile = File(...)):
-    caption = generate_image_caption(image)
+    caption = image_caption(image)
     return JsonResponse({'caption': caption})
--- storybook/llm_models/blip_instruct.py
+++ storybook/llm_models/blip_instruct.py
@@ -3,18 +3,14 @@ from PIL import Image as PILImage
 import torch
 import gc
 
-def complete_sentence(pil_image, prompt):
-    gc.collect()
-    torch.cuda.empty_cache()
+processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicuna-7b")
+model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-7b",load_in_8bit=True).to(device="cuda")#, load_in_8bit=True)
 
+def complete_sentence(pil_image, prompt):
     pil_image = PILImage.open(pil_image)
     pil_image = pil_image.convert("RGB").resize((512, 512))
-
-    processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicuna-7b")
-    model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-7b", load_in_8bit=True)
     inputs = processor(images=pil_image, text=prompt, return_tensors="pt").to(device="cuda")
 
-    # autoregressively generate an answer
     outputs = model.generate(
             **inputs,
             num_beams=5,
@@ -25,9 +21,45 @@ def complete_sentence(pil_image, prompt):
             length_penalty=1.0,
             temperature=1,
     )
-    #outputs[outputs == 0] = 2 # this line can be removed once https://github.com/huggingface/transformers/pull/24492 is fixed
     generated_text = processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
-    print(generated_text)
-    del processor
-    del model
+    return generated_text
+
+def continue_story(pil_image, prompt):
+    pil_image = PILImage.open(pil_image)
+    pil_image = pil_image.convert("RGB").resize((512, 512))
+    
+    prompt = "<Image> This is an image from a childrens book. Questions: Consider the following story:" + prompt +"What happens next? Answer:"
+    inputs = processor(images=pil_image, text=prompt, return_tensors="pt").to(device="cuda")
+
+    outputs = model.generate(
+        **inputs,
+        num_beams=5,
+        max_new_tokens=32,
+        min_length=1,
+        top_p=0.9,
+        repetition_penalty=100.0,
+        length_penalty=2.0,
+        temperature=1,
+)
+    generated_text = processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
+    return generated_text
+
+def image_caption(pil_image):
+    pil_image = PILImage.open(pil_image)
+    pil_image = pil_image.convert("RGB").resize((512, 512))
+    
+    prompt = "<Image> A short image description:"
+    inputs = processor(images=pil_image, text=prompt, return_tensors="pt").to(device="cuda")
+
+    outputs = model.generate(
+        **inputs,
+        num_beams=5,
+        max_new_tokens=32,
+        min_length=1,
+        top_p=0.9,
+        repetition_penalty=100.0,
+        length_penalty=2.0,
+        temperature=1,
+)
+    generated_text = processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
     return generated_text
