e2f75bee49ceda1add32328d332376a040a7a05b
dorj222 <dorj222@gmail.com>
2024-04-22 18:14:03 +0200

    Test small prompt changes in tinyllama

--- storybook/api/chat.py
+++ storybook/api/chat.py
@@ -7,9 +7,9 @@ from typing import Optional
 
 from storybook.schema import GenerateTextSchema, TranslateTextSchema, GenerateStorySchema
 from storybook.llm_models.tiny_llama import generate_title
-from storybook.llm_models.tiny_llama import generate_description_story
+from storybook.llm_models.tiny_llama import generate_description_story, complete_initial_sentence
 from storybook.llm_models.seamless import translate_text
-from storybook.llm_models.blip_instruct import complete_sentence, image_caption
+from storybook.llm_models.blip import generate_image_caption
 
 router = Router()
 @router.post("/titles")
@@ -23,7 +23,7 @@ def generate_translations(request, data: TranslateTextSchema):
     return JsonResponse({'generated_text': title_text})
 
 @router.post("/chapterstories")
-def generate_complete_sentence(request, ch_index: Optional[str] = None ,prompt: Optional[str] = None):
+def generate_chapter_prompt(request, ch_index: Optional[str] = None ,prompt: Optional[str] = None):
     generated_description = generate_description_story(user_input=prompt, ch_index=ch_index)
     response_data = {
         "generated_description": generated_description
@@ -31,9 +31,10 @@ def generate_complete_sentence(request, ch_index: Optional[str] = None ,prompt: 
     return JsonResponse(response_data)
 
 @router.post("/fullsentences")
-def generate_complete_sentence(request, image: UploadedFile = File(...), 
-                               prompt: Optional[str] = None):
-    generated_description = complete_sentence(pil_image=image,prompt=prompt) 
+def generate_chapter_completion(request, image: UploadedFile = File(...), prompt: Optional[str] = None, temperature: Optional[float] = None):
+    caption = generate_image_caption(image)
+
+    generated_description = complete_initial_sentence(user_input = prompt, img_caption=caption, temperature = temperature) 
     response_data = {
         "generated_description": generated_description
     } 
@@ -41,7 +42,7 @@ def generate_complete_sentence(request, image: UploadedFile = File(...),
 
 @router.post("/captions")
 def create_image_caption(request, image: UploadedFile = File(...)):
-    caption = image_caption(image)
+    caption = generate_image_caption(image)
     print("CAPTION: "  + caption)
 
     return JsonResponse({'caption': caption})
--- storybook/llm_models/tiny_llama.py
+++ storybook/llm_models/tiny_llama.py
@@ -34,7 +34,7 @@ def generate_description_story(user_input: str , ch_index: str) -> str:
     prompt = pipe.tokenizer.apply_chat_template(
         messages, tokenize=False, add_generation_prompt=False
     )
-    outputs = pipe(prompt, max_new_tokens=128, do_sample=True, temperature=0.7, top_k=50, top_p=0.95, repetition_penalty=1.2)
+    outputs = pipe(prompt, max_new_tokens=64, do_sample=True, temperature=0.7, top_k=50, top_p=0.95, repetition_penalty=1.3)
     # Extract the generated text
     generated_text = outputs[0]["generated_text"]
     generated_text = generated_text.split('\n<|assistant|>\n', 1)
@@ -54,6 +54,8 @@ def generate_description_story(user_input: str , ch_index: str) -> str:
         generated_text = '. '.join(sentences[:2]) + ". "
     else:
         generated_text = '. '.join(sentences[:3]) + ". " if len(sentences) > 2 else '.'.join(sentences)
+
+
     gc.collect()
     torch.cuda.empty_cache()
     return generated_text
@@ -89,27 +91,31 @@ def generate_title(user_input: str) -> str:
     else:
         return 'Title not generated'
     
-def complete_initial_sentence(user_input: str, img_caption: str, chapter: str) -> str:
+def complete_initial_sentence(user_input: str, img_caption: str, temperature: float) -> str:
     messages = [
         {
             "role": "system",
-            "content": config["complete_prompt_part1"],
+            "content": config["narrator_prompt_start"] ,
         },
-        {"role": "user", "content": user_input + " " + img_caption},
+        {"role": "user", "content": config["complete_prompt_part1"] + " \n Sentence1: " + user_input  + "\nSentence2" + img_caption}
     ]
     # Apply chat template and generate text
     prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)
-    outputs = pipe(prompt, max_new_tokens=64, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
-
+    outputs = pipe(prompt, max_new_tokens=64, do_sample=True, temperature=temperature, top_k=50, top_p=0.95)
+    print(outputs)
     # Extract the generated text
     generated_text = outputs[0]["generated_text"]
     # Find the assistant's response start index
     assistant_index = generated_text.find("<|assistant|>")
     gc.collect()
     torch.cuda.empty_cache()
+    
     if assistant_index != -1:
         # Get the assistant's response
         assistant_response = generated_text[assistant_index + len("<|assistant|>"):]
+        # Remove Complete keyword if present
+        if "Complete:" in assistant_response:
+            assistant_response=assistant_response.removeprefix('\nComplete:')
         return assistant_response
     else:
         return 'Text not generated'
